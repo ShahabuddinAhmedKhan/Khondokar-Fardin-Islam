3b63baf208358155a153d775b0adc09d7fb91c62
Zeusine <shawon2012.sk@gmail.com>
2025-07-27 19:12:08 +0600

    about

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='bg-white h-screen text-black font-space text-3xl md:text-6xl'>
+            <p className='max-w-5xl mx-auto text-center pt-40 px-5'>
+                I live where strategy meets creativity. By day, I decode audiences and build brands; by night, I turn stories into digital art. <span className='font-bold'>
+                    Whether it’s shaping perception through marketing or emotion through illustration, I’m all about creating work that connects.
+                </span>
+
+            </p>
+            
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,13 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Outlet } from 'react-router';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 const Home = () => {
   return (
     <>
     <Hero></Hero>
+    <About></About>
     <WorkGrid></WorkGrid>
     
     </>
